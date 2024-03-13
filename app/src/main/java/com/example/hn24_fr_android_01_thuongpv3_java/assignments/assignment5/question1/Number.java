14526c66d99396ec7416523ccc2ed97fd3b0370e
Tom <vanthuong00002@gmail.com>
2024-03-13 10:30:11 +0700

    fix assignment 5 question 1: add wait and notify

--- app/src/main/java/com/example/hn24_fr_android_01_thuongpv3_java/assignments/assignment5/question1/Number.java
+++ app/src/main/java/com/example/hn24_fr_android_01_thuongpv3_java/assignments/assignment5/question1/Number.java
@@ -2,22 +2,29 @@ package com.example.hn24_fr_android_01_thuongpv3_java.assignments.assignment5.qu
 
 import java.util.Random;
 
-public class Number {
+public class Number{
     private int number;
 
     public Number() {
 
     }
     public synchronized void printRandomNumber(){
-        System.out.print("Random number: ");
+        System.out.println("Random number: ");
         Random random = new Random();
         int randomNumber = random.nextInt(20) + 1;
         number = randomNumber;
+        notify();
         System.out.println(randomNumber);
     }
     public synchronized void printSquareNumber(){
-        System.out.print("Square number: ");
-        double squareNumber = Math.pow((double) number, 2);
+        double squareNumber;
+        try {
+            wait();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Square number: ");
+        squareNumber = Math.pow((double) number, 2);
         System.out.println(squareNumber);
 
     }
